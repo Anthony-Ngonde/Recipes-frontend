a49e9d04dcbf15c3145729a589e563022d3c39d9
Anthony Ngonde <anto.ngonde@gmail.com>
2024-10-02 12:20:48 +0300

    Login and SignUp forms created

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,9 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
+import { Form, FormControl, FormGroup, FormLabel, Button } from "react-bootstrap";
 
 const LoginPage=()=>{
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+
+    const loginUser=()=>{
+        console.log(username)
+        console.log(password)
+    }
+
+    setUsername('')
+    setPassword('')
+
+
     return(
-        <div className="home">
-            <h1>Login Page</h1>
+        <div className="container">
+            <div className="form">
+                <h1>Login Page</h1>
+                 <form>
+                    <FormGroup>
+                        <FormLabel>Username</FormLabel>
+                        <FormControl type="text" placeholder="Your username"
+                        value={username}
+                        name="username"
+                        onChange={(e)=>{setUsername(e.target.value)}}
+                        />
+                    </FormGroup>
+                    <br></br>
+                    <FormGroup>
+                        <FormLabel>Password</FormLabel>
+                        <FormControl type="password" placeholder="Your password"
+                         value={password}
+                        name="password"
+                        onChange={(e)=>{setPassword(e.target.value)}}
+                        />
+                    </FormGroup>
+                    <br></br>
+                    <FormGroup>
+                        <Button as="sub" variant="primary" onClick={loginUser}>Login</Button>
+                    </FormGroup>
+                </form>
+            </div>
         </div>
     )
 }
--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,8 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import { Form, FormControl, FormGroup, FormLabel, Button } from "react-bootstrap";
 
 
 const SignUpPage=()=>{
+    const [username, setUsername]=useState('')
+    const [email, setEmail]=useState('')
+    const [password, setPassword]=useState('')
+    const [confirmPassword, setConfirmPassword]=useState('')
+
+    const submitForm=()=>{
+        console.log("Form submitted");
+        console.log(username)
+        console.log(email)
+        console.log(password)
+        console.log(confirmPassword)
+
+        setUsername('')
+        setEmail('')
+        setPassword('')
+        setConfirmPassword('')
+    }
+
+
     return(
         <div className="container">
             <div className="form">
@@ -13,31 +32,39 @@ const SignUpPage=()=>{
                         <FormControl type="text" placeholder="Your username"
                         value={username}
                         name="username"
+                        onChange={(e)=>{setUsername(e.target.value)}}
                         />
                     </FormGroup>
+                    <br></br>
                     <FormGroup>
                         <FormLabel>Email</FormLabel>
                         <FormControl type="email" placeholder="Your email"
                          value={email}
                         name="email"
+                        onChange={(e)=>{setEmail(e.target.value)}}
                         />
                     </FormGroup>
+                    <br></br>
                     <FormGroup>
                         <FormLabel>Password</FormLabel>
                         <FormControl type="password" placeholder="Your password"
                          value={password}
                         name="password"
+                        onChange={(e)=>{setPassword(e.target.value)}}
                         />
                     </FormGroup>
+                    <br></br>
                     <FormGroup>
                         <FormLabel>Confirm Password</FormLabel>
                         <FormControl type="password" placeholder="Your password"
                          value={confirmPassword}
                         name="confirmPassword"
+                        onChange={(e)=>{setConfirmPassword(e.target.value)}}
                         />
                     </FormGroup>
+                    <br></br>
                     <FormGroup>
-                        <Button as="sub" variant="primary">SignUp</Button>
+                        <Button as="sub" variant="primary" onClick={submitForm}>SignUp</Button>
                     </FormGroup>
                 </form>
             </div>
