e5fab5a2d9ba37cfb1757ac9d51989e544e804e8
Anthony Ngonde <anto.ngonde@gmail.com>
2024-10-02 10:40:31 +0300

    Added NavBar component

--- src/index.js
+++ src/index.js
@@ -1,22 +1,34 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import React, {useEffect, useState} from 'react'
 import { createRoot } from 'react-dom/client'
 import ReactDOM from 'react-dom'
+import NavBar from './components/Navbar';
 
 
 const App=()=>{
 
+    // const [message, setMessage] = useState(''); 
 
-    useEffect(
-        ()=>{
-            fetch('/recipe/hello')
-            .then(response=>response.json())
-            .then(data=>console.log(data))
-        },[]
-    )
-    const [message, setMessage] = useState('')
-    return (
+   
+    // useEffect(() => {
+    //     fetch('/recipe/hello')
+    //         .then(response => response.json())
+    //         .then(data => {
+    //             console.log(data);
+    //             setMessage(data.message); 
+    //         })
+    //         .catch(err => console.log(err));
+    // }, []);
+   
+    // return (
+    //     <div className="app">
+    //         {message}
+    //     </div>
+    // )
+
+    return(
         <div className="app">
-            {message}
+          <NavBar/>
         </div>
     )
 }
