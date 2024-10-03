8ac62bfa360c984ed7e556f293cbc7995b39b91d
Anthony Ngonde <anto.ngonde@gmail.com>
2024-10-03 15:58:55 +0300

    Created a form on the create a recipe page

--- src/components/CreateRecipe.js
+++ src/components/CreateRecipe.js
@@ -1,9 +1,27 @@
 import React from "react";
+import { Form,Button, FormGroup, FormLabel, FormControl } from "react-bootstrap";
+
 
 const CreateRecipePage=()=>{
     return(
-        <div className="home">
-            <h1>CreateRecipe Page</h1>
+        <div className="container">
+            <h1>Create a Recipe</h1>
+            <form>
+                <FormGroup>
+                    <FormLabel>Title</FormLabel>
+                    <FormControl type="text"/>
+                </FormGroup>
+                <FormGroup>
+                    <FormLabel>Description</FormLabel>
+                    <FormControl as="textarea" rows={5}/>
+                </FormGroup>
+                <br></br>
+                <FormGroup>
+                    <Button variant="primary">
+                        Save
+                    </Button>
+                </FormGroup>
+            </form>
         </div>
     )
 }
