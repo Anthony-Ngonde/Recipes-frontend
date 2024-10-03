9282b88a8590d76c92d296336b8d0e382323b2de
Anthony Ngonde <anto.ngonde@gmail.com>
2024-10-03 15:39:56 +0300

    Fetched all recipes from backend to the console

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { useAuth } from "../auth";
 
 
 const LoggedinHome=()=>{
+    const [recipes, setRecipes] = useState([])
+
+    useEffect(
+        ()=>{
+            fetch('/recipe/recipes')
+            .then(res=>res.json())
+            .then(data=>console.log(data))
+            .catch(err=>console.log(err))
+        },[]
+    );
+
+
+
     return(
         <div className="recipes">
             <h1>List of Recipes</h1>
@@ -12,6 +25,7 @@ const LoggedinHome=()=>{
 }
 
 const LoggedOutHome=()=>{
+
     return(
         <div className="home container">
         <h1 className="heading">Welcome to the Recipes</h1>
