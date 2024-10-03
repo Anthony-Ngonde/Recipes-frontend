b1be56bffa92ee5612673cd8a21e4f86d20ecfb6
Anthony Ngonde <anto.ngonde@gmail.com>
2024-10-03 16:06:57 +0300

    Added validation on the create a recipe form

--- src/components/CreateRecipe.js
+++ src/components/CreateRecipe.js
@@ -1,20 +1,40 @@
 import React from "react";
 import { Form,Button, FormGroup, FormLabel, FormControl } from "react-bootstrap";
+import {useForm} from 'react-hook-form'
 
 
 const CreateRecipePage=()=>{
+
+    const {register, handleSubmit, reset, formState:{errors}} = useForm()
+
     return(
         <div className="container">
             <h1>Create a Recipe</h1>
             <form>
                 <FormGroup>
                     <FormLabel>Title</FormLabel>
-                    <FormControl type="text"/>
+                    <FormControl type="text"
+                    {...register('title', {required:true, maxLength:25})}
+
+                    />
                 </FormGroup>
+
+                {errors.title && <p style={{color:'red'}}><small>Title is required</small></p>}
+
+                {errors.title?.type === "maxLength" && <p style={{color:'red'}}><small>Title should be less than 25 characters</small></p>}
+                
                 <FormGroup>
                     <FormLabel>Description</FormLabel>
-                    <FormControl as="textarea" rows={5}/>
+                    <FormControl as="textarea" rows={5}
+                     {...register('description', {required:true, maxLength:255})}
+                     
+                    />
                 </FormGroup>
+
+                {errors.description && <p style={{color:'red'}}><small>Description is required</small></p>}
+
+                {errors.description?.type === "maxLength" && <p style={{color:'red'}}><small>Description should be less than 255 characters</small></p>}
+
                 <br></br>
                 <FormGroup>
                     <Button variant="primary">
